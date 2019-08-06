5d6a9f679f42cd005bc35e38d28948125a9d0325
chumakc <chumakc@pm.me>
2019-08-06 17:49:37 -0400

    Initial Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using LmsApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LmsApp.API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
