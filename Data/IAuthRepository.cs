6d2469b07f90d9e4ca990b371fda486fd08cdb35
Colin Chumak <chumakc@pm.me>
2019-08-20 18:04:26 -0400

    Created user model for storing passwords with hash+salt. Created the IAuthRepository interface and implementation. Created the methods for the IAuthRepository implementation. Registered the service in Startup.cs using AddScoped

---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using LmsApp.API.Models;
+
+namespace LmsApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
