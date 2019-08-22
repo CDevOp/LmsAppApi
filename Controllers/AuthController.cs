2eed24e0ed34cd6154368dbdaf99290feb8ec92e
Colin Chumak <chumakc@pm.me>
2019-08-22 13:10:50 -0400

    Added Login method which begins construction of JWT token usage

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using LmsApp.API.Data;
 using LmsApp.API.Dtos;
 using LmsApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LmsApp.API.Controllers
 {
@@ -11,8 +17,10 @@ namespace LmsApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
@@ -32,6 +40,53 @@ namespace LmsApp.API.Controllers
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            // Check if we have a user and if their username and password match what is in the database           
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                // Building up token with 2 claims
+
+                // Users Id
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+
+                // Users username
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            // In order to make sure the token is a valid token when it comes back, the server needs to sign
+            // the token. "AppSettings:Token" is declared in appsettings.json and should be a massive, 
+            // randomly generated string
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Create the token and pass in claims, give it expirey date, and pass in signing credentials
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // Write token in the response we send back to the client
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
