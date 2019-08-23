d84ef86892cd41e09b4907713da9cf09117106bb
Colin Chumak <chumakc@pm.me>
2019-08-23 18:15:53 -0400

    Removed custom error message

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -8,7 +8,7 @@ namespace LmsApp.API.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "Between 4 and 8")]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
