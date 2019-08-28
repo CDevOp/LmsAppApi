c6be1c23e5e9c177cfeb01dbb4f1e8aa5ba8d89a
Colin Chumak <chumakc@pm.me>
2019-08-27 20:03:13 -0400

    Finished work on ability to send photo to cloudinary

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -27,6 +27,8 @@ namespace LmsApp.API.Helpers
 
             CreateMap<Photo, PhotosForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }        
     }
 }
