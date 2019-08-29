b89513e05e064a2546c8b33aae65b73ad2c4b120
Colin Chumak <chumakc@pm.me>
2019-08-29 19:44:36 -0400

    Added filtering option for last active / created

--- Data/LmsRepository.cs
+++ Data/LmsRepository.cs
@@ -49,7 +49,8 @@ namespace LmsApp.API.Data
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users =  _context.Users.Include(p => p.Photos).AsQueryable();
+            var users =  _context.Users.Include(p => p.Photos)
+                .OrderByDescending(u => u.LastActive).AsQueryable();
 
             // Filter out current user from list of returned user
             users = users.Where(u => u.Id != userParams.UserId);
@@ -66,6 +67,21 @@ namespace LmsApp.API.Data
                 var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
 
                 users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
+
+            // Filter users based on when account created - default to last active
+            // if nothing supplied
+            if (!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy)
+                {
+                    case "created": 
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
             }
             
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
