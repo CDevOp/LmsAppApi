308f8d2bd55c54d027603508c0e84ef4194b8c04
Colin Chumak <chumakc@pm.me>
2019-08-31 16:26:14 -0400

    Added get likers/likees functionality for user

--- Data/LmsRepository.cs
+++ Data/LmsRepository.cs
@@ -66,6 +66,18 @@ namespace LmsApp.API.Data
             // If user is female, return males
             users = users.Where(u => u.Gender == userParams.Gender);
 
+            if (userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
+            }
+
+            if (userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
+
             // Filter users based on age - default to 18 - 99 if not specified 
             if (userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
@@ -91,6 +103,23 @@ namespace LmsApp.API.Data
             }
             
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+        }
+
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _context.Users
+                .Include(x => x.Likers)
+                .Include(x => x.Likees)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (likers)
+            {
+                return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+            }
+            else
+            {
+                return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
+            }
         }
 
         public async Task<bool> SaveAll()
